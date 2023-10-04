dd529d0a458274490c7dec85765f9fc00f58d288
ANDERSON NOGUEIRA <anderson.nogueira01@hotmail.com>
2023-10-04 00:07:54 -0400

    par-impar

---
+++ par-impar.py
@@ -0,0 +1,11 @@
+#projeto par ou impar
+while True:
+    try:
+        valor = int(input('Digite um valor: '))
+        if valor % 2 == 0:
+            print('Número Par')
+        
+        else:
+            print('Número Ímpar')
+    except:
+        print('Digite apenas números')
